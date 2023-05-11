30f85f817d85f1b05681a4c9a5270396efb13ac2
aliffadhil28 <alif70662@gmail.com>
2023-05-11 07:39:26 +0700

    first commit

---
+++ php/connect_db.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "root";
+$dbname = "arkatama_store";
+
+// Create connection
+$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "<h1>Data Pengguna</h1>";
+    
+}
